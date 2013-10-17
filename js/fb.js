5ddcf046c1e46ed63f06a35f34a7309e9dafc63f
willy <willypoteloin@yahoo.fr>
2013-10-17 21:02:14 +0200

    ajout du bouton de deconnexion

--- js/fb.js
+++ js/fb.js
@@ -1,38 +1,40 @@
- window.fbAsyncInit = function() {
+window.fbAsyncInit = function() {
     // init the FB JS SDK
     FB.init({
       appId      : '203651863118894',                    // App ID from the app dashboard
       channelUrl : 'http://poteloin.synology.me',               // Channel file for x-domain comms
       status     : true,                                 // Check Facebook Login status
       xfbml      : true                                  // Look for social plugins on the page
-  });
+    });
 
     // Additional initialization code such as adding Event Listeners goes here
     FB.Event.subscribe('auth.authResponseChange', function(response) {
-    // Here we specify what we do with the response anytime this event occurs. 
-    if (response.status === 'connected') {
-      // The response object is returned with a status field that lets the app know the current
-      // login status of the person. In this case, we're handling the situation where they 
-      // have logged in to the app.
-      retrieveInformation();
-  } else if (response.status === 'not_authorized') {
-      // In this case, the person is logged into Facebook, but not into the app, so we call
-      // FB.login() to prompt them to do so. 
-      // In real-life usage, you wouldn't want to immediately prompt someone to login 
-      // like this, for two reasons:
-      // (1) JavaScript created popup windows are blocked by most browsers unless they 
-      // result from direct interaction from people using the app (such as a mouse click)
-      // (2) it is a bad experience to be continually prompted to login upon page load.
-      FB.login();
-  } else {
-      // In this case, the person is not logged into Facebook, so we call the login() 
-      // function to prompt them to do so. Note that at this stage there is no indication
-      // of whether they are logged into the app. If they aren't then they'll see the Login
-      // dialog right after they log in to Facebook. 
-      // The same caveats as above apply to the FB.login() call here.
-      FB.login();
-  }
-});
+        // Here we specify what we do with the response anytime this event occurs. 
+        if (response.status === 'connected') {
+          // The response object is returned with a status field that lets the app know the current
+          // login status of the person. In this case, we're handling the situation where they 
+          // have logged in to the app.
+          retrieveInformation();
+        }
+        else if (response.status === 'not_authorized') {
+            // In this case, the person is logged into Facebook, but not into the app, so we call
+            // FB.login() to prompt them to do so. 
+            // In real-life usage, you wouldn't want to immediately prompt someone to login 
+            // like this, for two reasons:
+            // (1) JavaScript created popup windows are blocked by most browsers unless they 
+            // result from direct interaction from people using the app (such as a mouse click)
+            // (2) it is a bad experience to be continually prompted to login upon page load.
+            FB.login();
+        }
+        else {
+            // In this case, the person is not logged into Facebook, so we call the login() 
+            // function to prompt them to do so. Note that at this stage there is no indication
+            // of whether they are logged into the app. If they aren't then they'll see the Login
+            // dialog right after they log in to Facebook. 
+            // The same caveats as above apply to the FB.login() call here.
+            FB.login();
+        }
+    });
 };
 
   // Load the SDK asynchronously
@@ -47,7 +49,14 @@
 
 
   function retrieveInformation() {
-    FB.api('/me/',{fields: 'friends.fields(name,gender,birthday,relationship_status,picture,first_name,last_name,username)'}, function(response) {
+    FB.api('/me/',{fields: 'friends.fields(name,gender,birthday,relationship_status,picture,first_name,last_name,username,mutualfriends)'}, function(response) {
       console.log(response);
-  });
-}
+    });
+  }
+
+  function logout() {
+    FB.logout(function(response) {
+      alert('Vous avez bien été déconnecté !');
+    });
+  }
+
